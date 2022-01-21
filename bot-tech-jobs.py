667e709a02d4e66ebd35c1b855dcec3d8b84e571
FFR Developer <flaviofrancis@msn.com>
2022-01-21 19:32:38 +0100

    Create bot-tech-jobs.py

---
+++ bot-tech-jobs.py
@@ -0,0 +1,23 @@
+import auth
+import tweepy
+from datetime import datetime, timedelta
+
+def scrape(words, date_since, numtweet, language):
+
+    tweets = tweepy.Cursor(api.search_tweets,
+                    words, lang=language,
+                    since_id=date_since,
+                    tweet_mode='extended').items(numtweet)
+
+    list_tweets = [tweet for tweet in tweets]
+
+    for tweet in list_tweets:
+        status = api.get_status(tweet.id)
+        if not status.retweeted:
+            api.retweet(tweet.id)
+
+
+if __name__ == '__main__':   
+    api = auth.get_api()
+    d = datetime.today() - timedelta(days=14)
+    scrape(words='(vaga OR vagas) (pj OR clt)  (dev OR desenvolvedor OR UI OR UX OR DevOps OR SRE) R$ -filter:retweets', date_since=d ,numtweet=300, language='pt')    
